cbea629871e90331464d7236ed0eaf835492ebb4
Blake Winton <bwinton@latte.ca>
2019-05-22 15:41:52 -0400

    Start adding new images.

---
+++ 1/js/main.js
@@ -0,0 +1,50 @@
+(function() {
+    const ROW_COUNT = 5;
+    const COL_COUNT = 15;
+
+    let state = {
+        index: [0,0]
+    };
+
+    let move = direction => {
+        switch (direction) {
+        case 'up':
+            if (state.index[1] > 0) {
+                state.index[1] -= 1;
+            }
+            break;
+        case 'left':
+            if (state.index[0] > 0) {
+                state.index[0] -= 1;
+            }
+            break;
+        case 'right':
+            if (state.index[0] < COL_COUNT) {
+                state.index[0] += 1;
+            }
+            break;
+        case 'down':
+            if (state.index[1] < ROW_COUNT) {
+                state.index[1] += 1;
+            }
+            break;
+
+        default:
+            console.error(`Unknown direction!!! ${direction}`);
+            break;
+        }
+        console.log(direction, state.index);
+    }
+
+    $('.button').click(e => {
+        move(e.target.id);
+    });
+    $(document).keydown(e => {
+        if (!e.key.startsWith("Arrow")) {
+            return;
+        }
+        let key = e.key.replace("Arrow", "").toLowerCase();
+        move(key);
+    })
+
+}());
