104dec7c17ac8fd0a66711748abfd8f4af7c7e2d
Blake Winton <bwinton@latte.ca>
2019-05-23 10:04:06 -0400

    Get the background moving too.

--- 1/js/main.js
+++ 1/js/main.js
@@ -1,31 +1,37 @@
 (function() {
+    const BG_ROW_COUNT = 6;
     const ROW_COUNT = 1;
     const COL_COUNT = 4;
 
     let state = {
-        index: [0,0]
+        browser_index: 0,
+        index: {row: 0, col: 0}
     };
 
     let move = direction => {
         switch (direction) {
         case 'up':
-            if (state.index[1] > 0) {
-                state.index[1] -= 1;
+            if (state.index.row > 0) {
+                state.index.row -= 1;
+            } else if (state.browser_index > 0) {
+                state.browser_index -= 1;
             }
             break;
         case 'left':
-            if (state.index[0] > 0) {
-                state.index[0] -= 1;
+            if (state.index.col > 0) {
+                state.index.col -= 1;
             }
             break;
         case 'right':
-            if (state.index[0] < COL_COUNT) {
-                state.index[0] += 1;
+            if (state.index.col < COL_COUNT) {
+                state.index.col += 1;
             }
             break;
         case 'down':
-            if (state.index[1] < ROW_COUNT) {
-                state.index[1] += 1;
+            if (state.index.row < ROW_COUNT) {
+                state.index.row += 1;
+            } else if (state.browser_index < BG_ROW_COUNT) {
+                state.browser_index += 1;
             }
             break;
 
@@ -34,8 +40,10 @@
             break;
         }
         let style = $('.tv')[0].style;
-        style.setProperty('--carousel-column', state.index[0]);
-        style.setProperty('--carousel-row', state.index[1]);
+        style.setProperty('--carousel-column', state.index.col);
+        style.setProperty('--carousel-row', state.index.row);
+        style.setProperty('--browser-row', state.browser_index);
+        console.log(JSON.stringify(state));
     }
 
     $('.button').click(e => {
